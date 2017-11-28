a72018c3d5010448db537c027d1b20060513bbb8
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2017-11-28 10:11:55 +0100

    Added saveSong method

--- src/mytunes/dal/DAManager.java
+++ src/mytunes/dal/DAManager.java
@@ -8,6 +8,7 @@ package mytunes.dal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import mytunes.be.PlayList;
@@ -20,35 +21,52 @@ import mytunes.be.UserMedia;
 public class DAManager {
 
     private ConnectionManager cm = new ConnectionManager();
-    
     private List<PlayList> playlistList = new ArrayList();
-    
-    public List<UserMedia> getSongs(String filter) {
+
+    public List<UserMedia> getSongs(String filter) throws DAException {
         List<UserMedia> songList = new ArrayList();
-        
+
         try (Connection con = cm.getConnection()) {
             PreparedStatement pstatement = con.prepareStatement("SELECT * FROM Music WHERE title LIKE ?");
             pstatement.setString(1, "%" + filter + "%");
-
             ResultSet result = pstatement.executeQuery();
             while (result.next()) {
                 UserMedia tempSong = new UserMedia();
+                tempSong.setId(result.getInt("id"));
                 tempSong.setTitle(result.getString("title"));
                 tempSong.setArtist(result.getString("artist"));
                 tempSong.setCategory(result.getString("category"));
                 tempSong.setTime(result.getTime("time"));
                 tempSong.setPath(result.getString("path"));
-
                 songList.add(tempSong);
             }
         }
         catch (Exception e) {
+            throw new DAException(e.getMessage());
         }
-        
-        for (UserMedia userMedia : songList) {
-            System.out.println(userMedia.toString());
-        }
-        
         return songList;
+    }
+    
+    public void saveSongs(UserMedia song) throws DAException{
+        try (Connection con = cm.getConnection()) {
+            PreparedStatement pstatement = con.prepareStatement("INSERT INTO Music(title, artist, category, time, path)"
+                                                              + "VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            pstatement.setString(1, song.getTitle());
+            pstatement.setString(2, song.getArtist());
+            pstatement.setString(3, song.getCategory());
+            pstatement.setTime(4, song.getTime());
+            pstatement.setString(5, song.getPath());
+            int affected = pstatement.executeUpdate();
+            if (affected<1)
+                throw new DAException("Song could not be added");
+            
+            ResultSet rs = pstatement.getGeneratedKeys();
+            if (rs.next()) {
+                song.setId(rs.getInt(1));
+            }
+        }
+        catch (Exception e) {
+            throw new DAException(e.getMessage());
+        }
     }
 }
